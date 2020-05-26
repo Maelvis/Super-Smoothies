c02641ccc0b0e6f74505d6c1d13c776f7ce43b59
Maelvis <63719019+Maelvis@users.noreply.github.com>
2020-05-26 17:58:55 +0100

    Animation of logo on nav bar
    I animated the logo to change to the superhero logo when the mouse hovers over it. I applied the changes to all pages.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// Animating the logo on navigation bar
 document.addEventListener('DOMContentLoaded', function() {
+let logo = document.getElementById('logo');
+
+logo.addEventListener('mouseenter', function(event) {
+    event.target.src = 'SmoothieHutLogoSuper.png';
+})
+
+logo.addEventListener('mouseout', function(event) {
+    event.target.src = 'SmoothieHutLogoNav.png';
+});
 
 })
