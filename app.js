f75b97d8a2372ccbbcff8bff40cdb950017f8a82
Maelvis <63719019+Maelvis@users.noreply.github.com>
2020-05-30 14:02:22 +0100

    Interactive contact form
    Contact form which disappears and a thank you message appears when submitting.

--- app.js
+++ app.js
@@ -10,4 +10,32 @@ logo.addEventListener('mouseout', function(event) {
     event.target.src = 'SmoothieHutLogoNav.png';
 });
 
+// Reactive contact form
+let button = document.getElementsByClassName('btn')[0];
+let form = document.getElementsByTagName('form')[0];
+let parent = form.parentElement;
+
+
+
+function myFunction () {
+    var thankYou =  document.createElement('p')
+    thankYou.innerText = 'Thank you for your message!';
+    thankYou.style.fontSize = '25px'
+    thankYou.style.fontFamily = 'Playfair Display, serif'
+    thankYou.style.textAlign = 'center';
+    thankYou.style.alignItems = 'center';
+    parent.removeChild(form);
+    parent.appendChild(thankYou);
+};
+
+button.addEventListener('click', myFunction);
+
+
+
+
+
+
+
+
+
 })
