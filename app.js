5416aaac2286ed9fa41a2a66146f33a5ab6bac42
Maelvis <63719019+Maelvis@users.noreply.github.com>
2020-06-04 18:01:51 +0100

    Form validation

--- app.js
+++ app.js
@@ -14,22 +14,33 @@ logo.addEventListener('mouseout', function(event) {
 let button = document.getElementsByClassName('btn')[0];
 let form = document.getElementsByTagName('form')[0];
 let parent = form.parentElement;
+let name = document.getElementsByName('question')[0];
+let email = document.getElementsByName('question1')[0];
+let message = document.getElementsByName('question2')[0];
 
 
 
 function myFunction () {
-    var thankYou =  document.createElement('p')
-    thankYou.innerText = 'Thank you for your message!';
-    thankYou.style.fontSize = '25px'
-    thankYou.style.fontFamily = 'Playfair Display, serif'
-    thankYou.style.textAlign = 'center';
-    thankYou.style.alignItems = 'center';
-    parent.removeChild(form);
-    parent.appendChild(thankYou);
-};
+    if (name.value == "" || email.value  == "" || message.value  == "") {
+        var emptyField =  document.createElement('p')
+        emptyField.innerText = 'Please fill in all the fields';
+        emptyField.style.fontSize = '25px'
+        emptyField.style.fontFamily = 'Playfair Display, serif'
+        emptyField.style.alignItems = 'center';
+        form.appendChild(emptyField)
+    } else {
+        var thankYou =  document.createElement('p')
+        thankYou.innerText = 'Thank you!';
+        thankYou.style.fontSize = '25px'
+        thankYou.style.fontFamily = 'Playfair Display, serif'
+        thankYou.style.textAlign = 'center';
+        thankYou.style.alignItems = 'center';
+        parent.removeChild(form);
+        parent.appendChild(thankYou);
+    } 
+    }
 
 button.addEventListener('click', myFunction);
-
 
 
 
